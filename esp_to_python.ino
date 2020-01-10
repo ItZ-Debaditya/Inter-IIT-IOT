c942449afa40a5cbf3635cdfa0f4c070071f49c9
ItZ-Debaditya <53126412+ItZ-Debaditya@users.noreply.github.com>
2020-01-10 19:08:18 +0530

    Add files via upload

---
+++ esp_to_python.ino
@@ -0,0 +1,19 @@
+/* F5 TEST FOR ESP2PY
+ * Written by Junicchi
+ * https://github.com/KebabLord/esp_to_python
+ * It simply increases and returns a variable everytime a python req came */
+
+#include "ESP_MICRO.h" //Include the micro library 
+int testvariable = 0;
+String str= "48 49 50 51";
+void setup(){
+  Serial.begin(115200); // Starting serial port for seeing details
+  start("deboasus","12345678");  // EnAIt will connect to your wifi with given details
+}
+void loop(){
+  waitUntilNewReq();  //Waits until a new request from python come
+  /* increases index when a new request came*/
+  testvariable += 1;
+  //returnThisInt(testvariable); //Returns the data to python
+  returnThisStr(str);
+}
